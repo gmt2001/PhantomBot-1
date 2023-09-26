5c4fc2af86815cba6f6e8aaa2714643199257022
gmt2001 <garymt2001@hotmail.com>
2023-09-26 18:19:23 -0400

    Implement Invoke All/Any

--- source/com/gmt2001/util/concurrent/ExecutorService.java
+++ source/com/gmt2001/util/concurrent/ExecutorService.java
@@ -16,13 +16,18 @@
  */
 package com.gmt2001.util.concurrent;
 
+import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
 import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutionException;
 import java.util.concurrent.Executors;
 import java.util.concurrent.Future;
 import java.util.concurrent.RejectedExecutionException;
 import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.ScheduledFuture;
 import java.util.concurrent.TimeUnit;
+import java.util.concurrent.TimeoutException;
 
 /**
  * Provides an interface to a shared {@link ScheduledExecutorService}
@@ -154,6 +159,100 @@ public final class ExecutorService {
         }
 
         SCHEDULEDEXECUTOR.execute(command);
+    }
+
+    /**
+     * Executes the given tasks, returning a list of {@link Future} holding their status and results when all complete.
+     * {@link Future#isDone()} is {@code true} for each element of the returned list. Note that a <i>completed</i> task could have
+     * terminated either normally or by throwing an exception. The results of this method are undefined if the given collection is
+     * modified while this operation is in progress
+     *
+     * @param <T> the type of the values returned from the tasks
+     * @param tasks the collection of tasks
+     * @return a list of {@link Future} representing the tasks, in the same sequential order as produced by the iterator for
+     * the given task list, each of which has completed
+     * @throws InterruptedException if interrupted while waiting, in which case unfinished tasks are cancelled
+     * @throws NullPointerException if tasks or any of its elements are {@code null}
+     * @throws RejectedExecutionException if any task cannot be scheduled for execution
+     */
+    public static <T> List<Future<T>> invokeAll(Collection<? extends Callable<T>> tasks) throws InterruptedException {
+        if (shutdown) {
+            return Collections.emptyList();
+        }
+
+        return SCHEDULEDEXECUTOR.invokeAll(tasks);
+    }
+
+    /**
+     * Executes the given tasks, returning a list of {@link Future} holding their status and results when all complete or the timeout
+     * expires, whichever happens first. {@link Future#isDone()} is {@code true} for each element of the returned list. Note
+     * that a <i>completed</i> task could have terminated either normally or by throwing an exception. The results of this
+     * method are undefined if the given collection is modified while this operation is in progress
+     *
+     * @param <T> the type of the values returned from the tasks
+     * @param tasks the collection of tasks
+     * @param timeout the maximum time to wait
+     * @param unit the time unit of the timeout argument
+     * @return a list of {@link Future} representing the tasks, in the same sequential order as produced by the iterator for
+     * the given task list. If the operation did not time out, each task will have completed. If it did time out, some of
+     * these tasks will not have completed
+     * @throws InterruptedException if interrupted while waiting, in which case unfinished tasks are cancelled
+     * @throws NullPointerException if tasks, any of its elements, or unit are {@code null}
+     * @throws RejectedExecutionException if any task cannot be scheduled for execution
+     */
+    public static <T> List<Future<T>> invokeAll(Collection<? extends Callable<T>> tasks, long timeout, TimeUnit unit)
+        throws InterruptedException {
+        if (shutdown) {
+            return Collections.emptyList();
+        }
+
+        return SCHEDULEDEXECUTOR.invokeAll(tasks, timeout, unit);
+    }
+
+    /**
+     * Executes the given tasks, returning the result of one that has completed successfully (i.e., without throwing an exception),
+     * if any do. Upon normal or exceptional return, tasks that have not completed are cancelled. The results of this method are
+     * undefined if the given collection is modified while this operation is in progress
+     *
+     * @param <T> the type of the values returned from the tasks
+     * @param tasks the collection of tasks
+     * @return the result returned by one of the tasks
+     * @throws InterruptedException if interrupted while waiting
+     * @throws NullPointerException if tasks or any element task subject to execution is {@code null}
+     * @throws ExecutionException if no task successfully completes
+     * @throws RejectedExecutionException if any task cannot be scheduled for execution
+     */
+    public static <T> T invokeAny(Collection<? extends Callable<T>> tasks) throws InterruptedException, ExecutionException {
+        if (shutdown) {
+            return null;
+        }
+
+        return SCHEDULEDEXECUTOR.invokeAny(tasks);
+    }
+
+    /**
+     * Executes the given tasks, returning the result of one that has completed successfully (i.e., without throwing an exception),
+     * if any do before the given timeout elapses. Upon normal or exceptional return, tasks that have not completed are cancelled.
+     * The results of this method are undefined if the given collection is modified while this operation is in progress
+     *
+     * @param <T> the type of the values returned from the tasks
+     * @param tasks the collection of tasks
+     * @param timeout the maximum time to wait
+     * @param unit the time unit of the timeout argument
+     * @return the result returned by one of the tasks
+     * @throws InterruptedException if interrupted while waiting
+     * @throws NullPointerException if tasks, or unit, or any element task subject to execution is {@code null}
+     * @throws TimeoutException if the given timeout elapses before any task successfully completes
+     * @throws ExecutionException if no task successfully completes
+     * @throws RejectedExecutionException if any task cannot be scheduled for execution
+     */
+    public static <T> T invokeAny(Collection<? extends Callable<T>> tasks, long timeout, TimeUnit unit)
+        throws InterruptedException, ExecutionException, TimeoutException {
+        if (shutdown) {
+            return null;
+        }
+
+        return SCHEDULEDEXECUTOR.invokeAny(tasks, timeout, unit);
     }
 
     /**
