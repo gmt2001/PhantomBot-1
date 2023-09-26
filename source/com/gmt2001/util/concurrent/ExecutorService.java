0980a1e7edac27d3fbc25ed79ad6a55c6e833914
gmt2001 <garymt2001@hotmail.com>
2023-09-26 18:19:56 -0400

    Implement interfaces for Virtual Thread Executor submission

--- source/com/gmt2001/util/concurrent/ExecutorService.java
+++ source/com/gmt2001/util/concurrent/ExecutorService.java
@@ -35,17 +35,28 @@ import java.util.concurrent.TimeoutException;
  * @author gmt2001
  */
 public final class ExecutorService {
-
+    /**
+     * Shared {@link ScheduledExecutorService} implementing a thread pool
+     */
     private static final ScheduledExecutorService SCHEDULEDEXECUTOR = Executors.newScheduledThreadPool(4);
+    /**
+     * Shared {@link java.util.concurrent.ExecutorService} implementing virtual threads
+     * <p>
+     * **IMPL** currently points to {@link #SCHEDULEDEXECUTOR} until Java 21 is implemented
+     */
+    private static final java.util.concurrent.ExecutorService VIRTUALEXECUTOR = SCHEDULEDEXECUTOR;
+    /**
+     * Indicates if the executors are shutdown
+     */
     private static boolean shutdown = false;
 
     private ExecutorService() {
     }
 
     /**
-     * The backing {@link ScheduledExecutorService}
+     * A {@link ScheduledExecutorService} implementing a thread pool
      *
-     * @return the backing {@link ScheduledExecutorService}
+     * @return the {@link ScheduledExecutorService}
      */
     public static ScheduledExecutorService executorService() {
         return SCHEDULEDEXECUTOR;
@@ -53,13 +64,11 @@ public final class ExecutorService {
 
     /**
      * An {@link java.util.concurrent.ExecutorService} that provides virtual threads
-     * <p>
-     * **IMPL** currently returns {@link #executorService()} until Java 21 is implemented
      *
      * @return the virtual threads {@link java.util.concurrent.ExecutorService}
      */
     public static java.util.concurrent.ExecutorService virtualExecutorService() {
-        return executorService();
+        return VIRTUALEXECUTOR;
     }
 
     /**
@@ -315,6 +324,175 @@ public final class ExecutorService {
     }
 
     /**
+     * Executes the given command on a virtual thread at some time in the future. The command may execute in a new thread, in a pooled thread, or in the calling thread,
+     * at the discretion of the {@code Executor} implementation
+     *
+     * @param command the runnable task
+     * @throws RejectedExecutionException if this task cannot be accepted for execution
+     * @throws NullPointerException if command is {@code null}
+     */
+    public static void executeVirtual(Runnable command) {
+        if (shutdown) {
+            return;
+        }
+
+        VIRTUALEXECUTOR.execute(command);
+    }
+
+    /**
+     * Executes the given tasks on virtual threads, returning a list of {@link Future} holding their status and results when all complete.
+     * {@link Future#isDone()} is {@code true} for each element of the returned list. Note that a <i>completed</i> task could have
+     * terminated either normally or by throwing an exception. The results of this method are undefined if the given collection is
+     * modified while this operation is in progress
+     *
+     * @param <T> the type of the values returned from the tasks
+     * @param tasks the collection of tasks
+     * @return a list of {@link Future} representing the tasks, in the same sequential order as produced by the iterator for
+     * the given task list, each of which has completed
+     * @throws InterruptedException if interrupted while waiting, in which case unfinished tasks are cancelled
+     * @throws NullPointerException if tasks or any of its elements are {@code null}
+     * @throws RejectedExecutionException if any task cannot be scheduled for execution
+     */
+    public static <T> List<Future<T>> invokeAllVirtual(Collection<? extends Callable<T>> tasks) throws InterruptedException {
+        if (shutdown) {
+            return Collections.emptyList();
+        }
+
+        return VIRTUALEXECUTOR.invokeAll(tasks);
+    }
+
+    /**
+     * Executes the given tasks on virtual threads, returning a list of {@link Future} holding their status and results when all complete or the timeout
+     * expires, whichever happens first. {@link Future#isDone()} is {@code true} for each element of the returned list. Note
+     * that a <i>completed</i> task could have terminated either normally or by throwing an exception. The results of this
+     * method are undefined if the given collection is modified while this operation is in progress
+     *
+     * @param <T> the type of the values returned from the tasks
+     * @param tasks the collection of tasks
+     * @param timeout the maximum time to wait
+     * @param unit the time unit of the timeout argument
+     * @return a list of {@link Future} representing the tasks, in the same sequential order as produced by the iterator for
+     * the given task list. If the operation did not time out, each task will have completed. If it did time out, some of
+     * these tasks will not have completed
+     * @throws InterruptedException if interrupted while waiting, in which case unfinished tasks are cancelled
+     * @throws NullPointerException if tasks, any of its elements, or unit are {@code null}
+     * @throws RejectedExecutionException if any task cannot be scheduled for execution
+     */
+    public static <T> List<Future<T>> invokeAllVirtual(Collection<? extends Callable<T>> tasks, long timeout, TimeUnit unit)
+        throws InterruptedException {
+        if (shutdown) {
+            return Collections.emptyList();
+        }
+
+        return VIRTUALEXECUTOR.invokeAll(tasks, timeout, unit);
+    }
+
+    /**
+     * Executes the given tasks on virtual threads, returning the result of one that has completed successfully (i.e., without throwing an exception),
+     * if any do. Upon normal or exceptional return, tasks that have not completed are cancelled. The results of this method are
+     * undefined if the given collection is modified while this operation is in progress
+     *
+     * @param <T> the type of the values returned from the tasks
+     * @param tasks the collection of tasks
+     * @return the result returned by one of the tasks
+     * @throws InterruptedException if interrupted while waiting
+     * @throws NullPointerException if tasks or any element task subject to execution is {@code null}
+     * @throws ExecutionException if no task successfully completes
+     * @throws RejectedExecutionException if any task cannot be scheduled for execution
+     */
+    public static <T> T invokeAnyVirtual(Collection<? extends Callable<T>> tasks) throws InterruptedException, ExecutionException {
+        if (shutdown) {
+            return null;
+        }
+
+        return VIRTUALEXECUTOR.invokeAny(tasks);
+    }
+
+    /**
+     * Executes the given tasks on virtual threads, returning the result of one that has completed successfully (i.e., without throwing an exception),
+     * if any do before the given timeout elapses. Upon normal or exceptional return, tasks that have not completed are cancelled.
+     * The results of this method are undefined if the given collection is modified while this operation is in progress
+     *
+     * @param <T> the type of the values returned from the tasks
+     * @param tasks the collection of tasks
+     * @param timeout the maximum time to wait
+     * @param unit the time unit of the timeout argument
+     * @return the result returned by one of the tasks
+     * @throws InterruptedException if interrupted while waiting
+     * @throws NullPointerException if tasks, or unit, or any element task subject to execution is {@code null}
+     * @throws TimeoutException if the given timeout elapses before any task successfully completes
+     * @throws ExecutionException if no task successfully completes
+     * @throws RejectedExecutionException if any task cannot be scheduled for execution
+     */
+    public static <T> T invokeAnyVirtual(Collection<? extends Callable<T>> tasks, long timeout, TimeUnit unit)
+        throws InterruptedException, ExecutionException, TimeoutException {
+        if (shutdown) {
+            return null;
+        }
+
+        return VIRTUALEXECUTOR.invokeAny(tasks, timeout, unit);
+    }
+
+    /**
+     * Submits a Runnable task for execution on a virtual thread and returns a {@link Future} representing that task, whose {@link Future#get()} method will return {@code null}
+     * upon successful completion
+     *
+     * @param task the task to submit
+     * @return a {@link Future} representing pending completion of the task
+     * @throws RejectedExecutionException if the task cannot be scheduled for execution
+     * @throws NullPointerException if the task is {@code null}
+     */
+    public static Future<?> submitVirtual(Runnable task) {
+        if (shutdown) {
+            return null;
+        }
+
+        return VIRTUALEXECUTOR.submit(task);
+    }
+
+    /**
+     * Submits a Runnable task for execution on a virtual thread and returns a {@link Future} representing that task, whose {@link Future#get()} method will return the given result
+     * upon successful completion
+     *
+     * @param <T> the return type of result
+     * @param task the task to submit
+     * @param result the result to return
+     * @return a {@link Future} representing pending completion of the task
+     * @throws RejectedExecutionException if the task cannot be scheduled for execution
+     * @throws NullPointerException if the task is {@code null}
+     */
+    public static <T> Future<T> submitVirtual(Runnable task, T result) {
+        if (shutdown) {
+            return null;
+        }
+
+        return VIRTUALEXECUTOR.submit(task, result);
+    }
+
+    /**
+     * Submits a value-returning task for execution on a virtual thread and returns a {@link Future} representing the pending results of the task, whose {@link Future#get()} method
+     * will return the task's result upon successful completion
+     * <p>
+     * If you would like to immediately block waiting for a task, you can use constructions of the form {@code result = exec.submit(aCallable).get();}
+     * <p>
+     * Note: The {@link Executors} class includes a set of methods that can convert some other common closure-like objects, for example,
+     * {@link java.security.PrivilegedAction} to {@link Callable} form so they can be submitted
+     *
+     * @param <T> the return type of the callable
+     * @param task the task to submit
+     * @return a {@link Future} representing pending completion of the task
+     * @throws RejectedExecutionException if the task cannot be scheduled for execution
+     * @throws NullPointerException if the task is {@code null}
+     */
+    public static <T> Future<T> submitVirtual(Callable<T> task) {
+        if (shutdown) {
+            return null;
+        }
+
+        return VIRTUALEXECUTOR.submit(task);
+    }
+
+    /**
      * Initiates an orderly shutdown in which previously submitted tasks are executed, but no new tasks will be accepted. Invocation has no additional
      * effect if already shut down
      *
@@ -325,6 +503,7 @@ public final class ExecutorService {
     public static void shutdown() {
         shutdown = true;
         SCHEDULEDEXECUTOR.shutdown();
+        VIRTUALEXECUTOR.shutdown();
     }
 
     /**
